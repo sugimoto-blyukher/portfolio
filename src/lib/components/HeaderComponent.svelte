7ed7a583643c6d34b1982fecd580b444b38861da
sinta <ramenrice555@gmail.com>
2025-07-28 21:59:50 +0900

    追加
    追加

--- src/lib/components/HeaderComponent.svelte
+++ src/lib/components/HeaderComponent.svelte
@@ -6,6 +6,6 @@
     <h1 class="text-8xl dotgothic16-regular"><a href="/">sinta</a></h1>
     <div class="mt-16 grid grid-cols-3">
         <h2 class="text-4xl dotgothic16-regular text-center"><a href="blog">Blog</a></h2>
-        <h2 class="text-4xl dotgothic16-regular text-center"><a href="">Projects</a></h2>
+        <h2 class="text-4xl dotgothic16-regular text-center"><a href="#">Projects</a></h2>
     </div>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,8 @@
 <div>
     <HeaderComponent />
-    <div>
-        
-    </div>
+    <section class="dotgothic16-regular bg-amber-300 grid grid-cols justify-center">
+        <h1>sinta</h1>
+    </section>
 </div>
 
 <script>
