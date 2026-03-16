a1e721833f16c923d0880875b775ba6dceadd52b
sugimoto-blyukher <ramenrice555@gmail.com>
2026-03-16 22:37:10 +0900

    fix: 不要な目標リンクをコメントアウト

--- src/lib/data/navigation.ts
+++ src/lib/data/navigation.ts
@@ -6,7 +6,7 @@ export interface NavigationLink {
 export const navigationLinks: NavigationLink[] = [
 	{ href: '/', label: 'About me' },
 	{ href: '/timeline', label: 'タイムライン' },
-	{ href: '/goal.html', label: '目標' },
+	//{ href: '/goal.html', label: '目標' },
 	{ href: '/projects', label: 'プロジェクト集' },
 	{ href: '/links', label: '関連リンク集' },
 	{ href: 'https://sugimoskaya.pages.dev/', label: 'ブログ'}
