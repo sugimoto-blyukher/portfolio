2f1de464e2e72c1ec7eba5d906885f0ca4cc6436
sugimoto-blyukher <ramenrice555@gmail.com>
2026-03-16 23:01:16 +0900

    fix: プロジェクトの説明を詳細化し、アジプロジェネレーターのURLを修正

--- src/lib/data/projects.ts
+++ src/lib/data/projects.ts
@@ -8,14 +8,20 @@ export interface Projects {
 export const projects: Projects[] = [
   {
     title: 'Sugimotos',
-    description: '私が現在開発を進めている自作OSです。Cで開発しています。現在はブートローダーとカーネルの開発を進めています。',
+    description: '私が現在開発を進めているモノリシックカーネルの自作OSです。LLMの力を借りつつCで開発しています。現在カーネルランドとユーザーランドの実装まで完了しました。次は微調整の段階です',
     url: 'https://github.com/sugimoto-blyukher/sugimotos',
     tags: ['OS', 'C', 'Kernel']
   },
   {
     title: 'アジプロジェネレーター',
-    description: '私が作ったソ連風のアジプロ文を生成するサイトです。(今はもう使えない)',
-    url: 'null',
+    description: '私が作ったソ連風のアジプロ文を生成するサイトです(今はもう使えない)。開発言語はpythonで、使用したライブラリとフレームワークはgemini apiとfastapiです',
+    url: 'https://github.com/sugimoto-blyukher/ajipuro/tree/old',
     tags: ['Python', 'FastAPI', 'GeminiAPI']
   },
+  {
+    title: 'キラキラネームメーカーのバックエンド',
+    description: '学内ハッカソンのp2hacksで技術者保育園のバックエンド担当として開発したものです。ESP32と通信して送られてきたデータを元にランダムでキラキラネームをGemini APIで生成し、プリンターで印刷するためのバックエンドです。',
+    url: 'https://github.com/sugimoto-blyukher/kirakiraname',
+    tags: ['Python', 'FastAPI', 'GeminiAPI', 'WeasyPrint']
+  }
 ]
